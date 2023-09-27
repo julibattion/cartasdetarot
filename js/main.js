c24e07895a209ce56089c1000ad8ed044f38122c
Juliana Battion <julibattion@gmail.com>
2023-09-27 20:40:41 -0300

    entrega-2

--- js/main.js
+++ js/main.js
@@ -1,66 +1,69 @@
+const cartasTarot = [{ 
+    nombre: "El Mago", 
+    significado: "Comienzo de algo nuevo" 
+}, 
+{ 
+    nombre: "La Sacerdotisa", 
+    significado: "Buen juicio y una intuición poderosa." 
+},
+{ 
+    nombre: "La Emperatriz", 
+    significado: "Poder femenino, fertilidad y atractivo." 
+},
+{ 
+    nombre: "El Emperador", 
+    significado: "Estabilidad, el poder terrenal y el control del propio ego." 
+},
+{ 
+    nombre: "Los Enamorados", 
+    significado: "Armonía, amor, confianza, honor, gozo y satisfacción de los deseos." 
+},
+{ 
+    nombre: "La Justicia", 
+    significado: "Justicia, equilibrio, honor, imparcialidad y buenas intenciones." 
+},
+{ 
+    nombre: "La Rueda de la Fortuna", 
+    significado: "Representa el destino, la capacidad de llegar al final de un problema." 
+},
+{ 
+    nombre: "La Fuerza", 
+    significado: "Triunfo de la mente sobre la materia y la fuerza física." 
+},
+{ 
+    nombre: "El Colgado", 
+    significado: "Sufrimiento y vergüenza, pérdida de ego, autoestima y riqueza material." 
+},
+{ 
+    nombre: "La Muerte", 
+    significado: "Transformación, final de relación, un trabajo o un ingreso, que es necesario antes de empezar de nuevo." 
+}];
+
 function cartaAleatoria() {
-    const carta1 = "El Mago";
-    const carta2 = "La Emperatriz";
-    const carta3 = "El Emperador";
-    const carta4 = "El Loco";
-    const carta5 = "El Colgado";
-    const carta6 = "La Muerte";
-    const carta7 = "La Fuerza";
-    const carta8 = "La Estrella";
-    const carta9 = "El Sol";
-    const carta10 = "La Luna";
-    const carta11 = "El Mundo";
-    const carta12 = "El Juicio";
-    const carta13 = "La Justicia";
-
-    const numeroAleatorio = Math.floor(Math.random() * 13) + 1;
-
-    switch (numeroAleatorio) {
-        case 1:
-            return carta1;
-        case 2:
-            return carta2;
-        case 3:
-            return carta3;
-        case 4:
-            return carta4;
-        case 5:
-            return carta5;
-        case 6:
-            return carta6;
-        case 7:
-            return carta7;
-        case 8:
-            return carta8;
-        case 9:
-            return carta9;
-        case 10:
-            return carta10;
-        case 11:
-            return carta11;
-        case 12:
-            return carta12;
-        case 13:
-            return carta13;
-    }
+    const numeroAleatorio = Math.floor(Math.random() * cartasTarot.length);
+    return cartasTarot[numeroAleatorio];
 }
 
 function jugarTarot() {
     console.log("Proba tu suerte con las cartas de tarot!");
 
-    let cartasSacadas = 0;
+    const respuesta = prompt("Presiona 'aceptar' para sacar 3 cartas o escribi 'salir' para terminar el juego:");
 
-    while (cartasSacadas < 3) {
-        const respuesta = prompt("Presiona 'aceptar' para sacar 3 cartas o escribi 'salir' para terminar el juego:");
-
-        if (respuesta === "salir") {
-            break;
-        }
-
-        const carta = cartaAleatoria();
-        console.log(`Tu carta es: ${carta}`);
-        cartasSacadas++;
+    if (respuesta === "salir") {
+        return;
     }
+
+    const cartasSeleccionadas = [];
+
+    for (let i = 0; i < 3; i++) {
+        const carta = cartaAleatoria();
+        cartasSeleccionadas.push(carta);
+    }
+
+    cartasSeleccionadas.forEach(carta => {
+        console.log(`Carta: ${carta.nombre}`);
+        console.log(`Significado: ${carta.significado}`);
+    });
 }
 
 jugarTarot();
