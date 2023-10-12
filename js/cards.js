afee95172aa75d6da0ed9fd6959bf6e9ad7512a5
Juliana Battion <julibattion@gmail.com>
2023-10-11 22:07:12 -0300

    entrega 3

---
+++ js/cards.js
@@ -0,0 +1,156 @@
+const cards = [
+    {
+        "id": 0,
+        "firstName": "El carro",
+        "description": "Establece la lucha, la perseverancia y los viajes. Es la señal del esfuerzo y los triunfos.",
+        "image": "chariot.jpg",
+        "score": 1
+    },
+    {
+        "id": 1,
+        "firstName": "La muerte",
+        "description": "Sugiere los finales, los procesos, transiciones y los cambios. Está relacionado con la transmutación y capacidad de reinventarse.",
+        "image": "death.jpg",
+        "score": 2
+    },
+    {
+        "id": 2,
+        "firstName": "El diablo",
+        "description": "Establece la revelación y la experimentación, la búsqueda de diferenciarse y el gran potencial.",
+        "image": "devil.jpg",
+        "score": 3
+    },
+    {
+        "id": 3,
+        "firstName": "El emperador",
+        "description": "Representa el liderazgo, los bienes materiales y el poder. Está relacionado con el posicionamiento social y la estabilidad financiera.",
+        "image": "emperor.jpg",
+        "score": 4
+    },
+    {
+        "id": 4,
+        "firstName": "La emperatriz",
+        "description": "Es vista como la carta de la belleza y la abundancia. Asimismo, su aparición puede comprender la fertilidad o nuevos inicios.",
+        "image": "empress.jpg",
+        "score": 5
+    },
+    {
+        "id": 5,
+        "firstName": "El loco",
+        "description": "Es una carta aventurera, que marca nuevos inicios. Representa la espontaneidad, el juego, la libertad e imaginación.",
+        "image": "fool.jpg",
+        "score": 6
+    },
+    {
+        "id": 6,
+        "firstName": "La rueda de la fortuna",
+        "description": "Es el símbolo de los cambios y el destino. Lo que debe ser, lo inevitable y las nuevas oportunidades en la vida.",
+        "image": "fortune.jpg",
+        "score": 7
+    },
+    {
+        "id": 7,
+        "firstName": "El colgado",
+        "description": "Es el desinterés y la independencia, muchas veces visto como egocentrismo.",
+        "image": "hangedman.jpg",
+        "score": 8
+    },
+    {
+        "id": 8,
+        "firstName": "El ermitaño",
+        "description": "Demuestra la soledad e introspección. Es el pensamiento, como también el miedo.",
+        "image": "hermit.jpg",
+        "score": 9
+    },
+    {
+        "id": 9,
+        "firstName": "El hierofante",
+        "description": "Representa a una persona comprometida con lo que quiere, que sabe mantener su integridad a pesar de la crítica, capaz de iluminar a otros con su ejemplo y enseñanzas.",
+        "image": "hierophant.jpg",
+        "score": 10
+    },
+    {
+        "id": 10,
+        "firstName": "El juicio",
+        "description": "Simboliza las cuentas pendientes y la necesidad de reflexionar sobre el pasado para poder avanzar.",
+        "image": "judgement.jpg",
+        "score": 11
+    },
+    {
+        "id": 11,
+        "firstName": "La justicia",
+        "description": "Es la mayor expresión de equidad. Establece el karma y la reflexión sobre las consecuencias de las acciones.",
+        "image": "justice.jpg",
+        "score": 12
+    },
+    {
+        "id": 12,
+        "firstName": "Los enamorados",
+        "description": "Simboliza las relaciones, el amor y las decisiones. Es una carta que señala emociones fuertes.",
+        "image": "lovers.jpg",
+        "score": 13
+    },
+    {
+        "id": 13,
+        "firstName": "El mago",
+        "description": "Representa los orígenes. Simboliza la capacidad de crear, la imaginación, inteligencia, creatividad y confianza en sí mismo.",
+        "image": "magician.jpg",
+        "score": 14
+    },
+    {
+        "id": 14,
+        "firstName": "La luna",
+        "description": "Brinda reflexión, señala inseguridades y dudas. Asimismo, puede traer al presente asuntos del pasado.",
+        "image": "moon.jpg",
+        "score": 15
+    },
+    {
+        "id": 15,
+        "firstName": "La suma sacerdotisa",
+        "description": "Comprende la intuición y los misterios del mundo. Puede representar la introspección, lo que no se ve y la inactividad.",
+        "image": "priestess.jpg",
+        "score": 16
+    },
+    {
+        "id": 16,
+        "firstName": "La estrella",
+        "description": "Señala el espíritu de juventud, la suerte y las emociones. Está relacionada con la espiritualidad y la fe, la confianza y la esperanza.",
+        "image": "star.jpg"
+        ,"score": 17
+    },
+    {
+        "id": 17,
+        "firstName": "La fuerza",
+        "description": "Son las capacidades y herramientas que una persona cuenta para lograr lo que desea o solucionar problemas.",
+        "image": "strength.jpg",
+        "score": 18
+    },
+    {
+        "id": 18,
+        "firstName": "El sol",
+        "description": "Comprende el éxito y el optimismo, el renacer desde la alegría. Es vista como una carta de mucha energía y con gran potencial.",
+        "image": "sun.jpg",
+        "score": 19
+    },
+    {
+        "id": 19,
+        "firstName": "La templanza",
+        "description": "Establece el control y la determinación a la hora de accionar. Puede representar el poder y la fuerza en ciertas situaciones.",
+        "image": "temperance.jpg",
+        "score": 20
+    },
+    {
+        "id": 20,
+        "firstName": "La torre",
+        "description": "Representa un nuevo punto de vista, la contemplación desde lo alto. Señala libertad y capacidad de ver absolutamente todo lo que sucede.",
+        "image": "tower.jpg",
+        "score": 21
+    },
+    {
+        "id": 21,
+        "firstName": "El mundo",
+        "description": "Establece el mundo terrenal y las acciones concretas. Invita a dejar de lado la imaginación y llevar a cabo acciones en la vida real.",
+        "image": "world.jpg",
+        "score": 22
+    }
+]
--- js/main.js
+++ js/main.js
@@ -1,69 +1,114 @@
-const cartasTarot = [{ 
-    nombre: "El Mago", 
-    significado: "Comienzo de algo nuevo" 
-}, 
-{ 
-    nombre: "La Sacerdotisa", 
-    significado: "Buen juicio y una intuición poderosa." 
-},
-{ 
-    nombre: "La Emperatriz", 
-    significado: "Poder femenino, fertilidad y atractivo." 
-},
-{ 
-    nombre: "El Emperador", 
-    significado: "Estabilidad, el poder terrenal y el control del propio ego." 
-},
-{ 
-    nombre: "Los Enamorados", 
-    significado: "Armonía, amor, confianza, honor, gozo y satisfacción de los deseos." 
-},
-{ 
-    nombre: "La Justicia", 
-    significado: "Justicia, equilibrio, honor, imparcialidad y buenas intenciones." 
-},
-{ 
-    nombre: "La Rueda de la Fortuna", 
-    significado: "Representa el destino, la capacidad de llegar al final de un problema." 
-},
-{ 
-    nombre: "La Fuerza", 
-    significado: "Triunfo de la mente sobre la materia y la fuerza física." 
-},
-{ 
-    nombre: "El Colgado", 
-    significado: "Sufrimiento y vergüenza, pérdida de ego, autoestima y riqueza material." 
-},
-{ 
-    nombre: "La Muerte", 
-    significado: "Transformación, final de relación, un trabajo o un ingreso, que es necesario antes de empezar de nuevo." 
-}];
+let sections = document.querySelectorAll('.section')
+let form = document.getElementById('form')
+let lectura = document.getElementById ('lectura')
+let cardsTarot = document.querySelector('.carousel-inner')
+let cardsInner = document.getElementById ('cards-inner')
+let verResultados = document.getElementById('verResultados')
+let volverAJugar = document.getElementById('volverAJugar')
+let firstScreen = document.getElementById('firstScreen')
+let resultadosModal = document.getElementById('resultadosModal')
+let carousel = document.getElementById('carrousel')
+let btnIrAlInicio = document. getElementById('btnIrAlInicio')
+let player1 = ''
+let tempCards = []
+let partidasGuardadas = []
 
-function cartaAleatoria() {
-    const numeroAleatorio = Math.floor(Math.random() * cartasTarot.length);
-    return cartasTarot[numeroAleatorio];
-}
 
-function jugarTarot() {
-    console.log("Proba tu suerte con las cartas de tarot!");
 
-    const respuesta = prompt("Presiona 'aceptar' para sacar 3 cartas o escribi 'salir' para terminar el juego:");
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    player1 = e.target.player1.value
+    
+    showPages('animacion')
 
-    if (respuesta === "salir") {
-        return;
-    }
+    form.reset()
 
-    const cartasSeleccionadas = [];
-
-    for (let i = 0; i < 3; i++) {
-        const carta = cartaAleatoria();
-        cartasSeleccionadas.push(carta);
-    }
-
-    cartasSeleccionadas.forEach(carta => {
-        console.log(`Carta: ${carta.nombre}`);
-        console.log(`Significado: ${carta.significado}`);
+    const nombreJugador = {
+        player1,
+        cartas: []
+        };
+    
+        partidasGuardadas.push(nombreJugador);
+    
+        localStorage.setItem('partidasGuardadas', JSON.stringify(partidasGuardadas));
+    
+        form.reset();
     });
+
+
+    if (localStorage.getItem('partidasGuardadas')) {
+        partidasGuardadas = JSON.parse(localStorage.getItem('partidasGuardadas'));
+    }
+
+
+const showPages = (page) => {
+    sections.forEach((item) => {
+        if (item.id === page) {
+            item.classList.remove('display-none')
+        } else {
+            item.classList.add('display-none')
+        }
+
+    })
+
+    if (page === 'animacion') {
+        let sto = setTimeout (() => iniciarJuego(), 3000);
+    }
 }
 
-jugarTarot();
+
+const iniciarJuego = () => {
+
+    showPages('carrousel')
+
+    let newCards = []
+    let random = ''
+    let cardsTarot = ''
+    let active = ''
+
+    while (newCards.length < 3) {
+        random = Math.floor(Math.random()* cards.length)
+        if (newCards.indexOf(cards[random]) == -1) {
+            newCards.push(cards[random]);        
+        }
+    }
+
+    partidasGuardadas[partidasGuardadas.length - 1].cartas = newCards;
+
+
+    localStorage.setItem('partidasGuardadas', JSON.stringify(partidasGuardadas));
+
+    tempCards += newCards;
+
+    newCards.forEach((card, index) => {
+
+
+        if (index === 0) active = 'active'
+        else active = '';    
+    
+        cardsTarot += `<div class= "carousel-item ${active}" id="card${index + 1}" data-number="${index + 1}">
+        <img src="./images/${card.image}" class="d-flex w-100 img-fluid"/>
+        <div class="carousel-caption">
+        <h5 class="firstName">${card.firstName}</h5>
+        <p class="description">${card.description}</p>
+        </div>
+        </div>`
+
+    });
+
+    cardsInner.innerHTML += cardsTarot;
+
+}
+
+
+
+let resModal = new bootstrap.Modal(document.getElementById ('resultadosModal'), {
+    keyboard: false
+})
+
+volverAJugar.addEventListener('click', (e) => {
+    resModal.hide();
+    firstScreen.classList.remove('display-none')
+    carousel.classList.add('display-none')
+
+});
