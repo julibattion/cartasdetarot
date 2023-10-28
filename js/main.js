0d3e1fb640978a09742b6a19213581941859d1ef
Juliana Battion <julibattion@gmail.com>
2023-10-27 22:58:21 -0300

    entrega final

--- js/main.js
+++ js/main.js
@@ -14,101 +14,120 @@ let tempCards = []
 let partidasGuardadas = []
 
 
-
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    player1 = e.target.player1.value
-    
-    showPages('animacion')
+    e.preventDefault();
+    player1 = e.target.player1.value;
 
-    form.reset()
+    showPages('animacion');
+
+    form.reset();
 
     const nombreJugador = {
         player1,
         cartas: []
-        };
-    
-        partidasGuardadas.push(nombreJugador);
-    
-        localStorage.setItem('partidasGuardadas', JSON.stringify(partidasGuardadas));
-    
-        form.reset();
-    });
+    };
 
+    partidasGuardadas.push(nombreJugador);
 
-    if (localStorage.getItem('partidasGuardadas')) {
-        partidasGuardadas = JSON.parse(localStorage.getItem('partidasGuardadas'));
-    }
+    localStorage.setItem('partidasGuardadas', JSON.stringify(partidasGuardadas));
+    form.reset();
 
+    setTimeout(() => {
+        fetch('/js/cards.json')
+            .then((response) => response.json())
+            .then((cardsData) => {
+                iniciarJuego(cardsData);
+            })
+            .catch((error) => {
+                console.error('Error al cargar las cartas:', error);
+            });
+    }, 3500);
+});
+
+if (localStorage.getItem('partidasGuardadas')) {
+    partidasGuardadas = JSON.parse(localStorage.getItem('partidasGuardadas'))
+};
 
 const showPages = (page) => {
     sections.forEach((item) => {
         if (item.id === page) {
-            item.classList.remove('display-none')
+            item.classList.remove('display-none');
+            if (page === 'animacion') {
+                Toastify({
+                    text: "Esto puede tardar unos segundos",
+                    duration: 3000,
+                    style: {
+                        background: "#dcc69f",
+                    },
+                    offset: {
+                        x: 528,
+                        y: 15,
+                    },
+                }).showToast();
+            }
         } else {
-            item.classList.add('display-none')
+            item.classList.add('display-none');
         }
+    });
+};
 
-    })
+const iniciarJuego = (cartas) => {
+    showPages('carrousel');
 
-    if (page === 'animacion') {
-        let sto = setTimeout (() => iniciarJuego(), 3000);
-    }
-}
+    const newCards = cartasAleatorias(cartas);
 
+    actualizarPartida(newCards);
 
-const iniciarJuego = () => {
+    guardarPartidas(partidasGuardadas);
 
-    showPages('carrousel')
+    carrousel(newCards);
+};
 
-    let newCards = []
-    let random = ''
-    let cardsTarot = ''
-    let active = ''
-
-    while (newCards.length < 3) {
-        random = Math.floor(Math.random()* cards.length)
-        if (newCards.indexOf(cards[random]) == -1) {
-            newCards.push(cards[random]);        
+const cartasAleatorias = (cartas) => {
+    const nuevasCartas = [];
+    while (nuevasCartas.length < 3) {
+        const random = Math.floor(Math.random() * cartas.length);
+        if (nuevasCartas.indexOf(cartas[random]) === -1) {
+            nuevasCartas.push(cartas[random]);
         }
     }
+    return nuevasCartas;
+};
 
-    partidasGuardadas[partidasGuardadas.length - 1].cartas = newCards;
+const actualizarPartida = (nuevasCartas) => {
+    partidasGuardadas[partidasGuardadas.length - 1].cartas = nuevasCartas;
+};
 
+const guardarPartidas = (partidas) => {
+    localStorage.setItem('partidasGuardadas', JSON.stringify(partidas));
+};
 
-    localStorage.setItem('partidasGuardadas', JSON.stringify(partidasGuardadas));
+const carrousel = (nuevasCartas) => {
+    let cardsTarot = '';
+    let active = '';
 
-    tempCards += newCards;
+    nuevasCartas.forEach((card, index) => {
+        if (index === 0) active = 'active';
+        else active = '';
 
-    newCards.forEach((card, index) => {
-
-
-        if (index === 0) active = 'active'
-        else active = '';    
-    
-        cardsTarot += `<div class= "carousel-item ${active}" id="card${index + 1}" data-number="${index + 1}">
+        cardsTarot += `<div class="carousel-item ${active}" id="card${index + 1}" data-number="${index + 1}">
         <img src="./images/${card.image}" class="d-flex w-100 img-fluid"/>
         <div class="carousel-caption">
         <h5 class="firstName">${card.firstName}</h5>
         <p class="description">${card.description}</p>
         </div>
-        </div>`
-
+        </div>`;
     });
 
     cardsInner.innerHTML += cardsTarot;
-
-}
-
-
+};
 
 let resModal = new bootstrap.Modal(document.getElementById ('resultadosModal'), {
     keyboard: false
-})
+});
 
 volverAJugar.addEventListener('click', (e) => {
     resModal.hide();
-    firstScreen.classList.remove('display-none')
-    carousel.classList.add('display-none')
-
+    firstScreen.classList.remove('display-none');
+    carousel.classList.add('display-none');
 });
